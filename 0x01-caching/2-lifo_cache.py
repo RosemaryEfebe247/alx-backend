e0aa61d8c1106fe32a0d89744bcc19c0a5e0243f
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-26 00:25:24 +0100

    LIFO

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" The fuction that gets and put an item in LIFO
+format
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ A class that initializes the put and get
+    in Last in first out format
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """ Appends key value to cache_data
+        Discards the first item if Max is reached
+        """
+        if key is None or item is None:
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discarded = list(self.cache_data.keys())[-1]
+            print(f"DISCARD: {discarded}")
+            del self.cache_data[discarded]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Funtion to get a key from cache_data
+        """
+        return self.cache_data.get(key, None)
