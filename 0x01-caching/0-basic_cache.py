bdd8ca654078180a76fc5a0d385d9924522da70a
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-10-25 05:51:02 +0100

    basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -15,6 +15,4 @@ class BasicCache(BaseCaching):
 
     def get(self, key):
         """ Function to get an item from the dictionary """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data.get(key)
+        return self.cache_data.get(key, None)
