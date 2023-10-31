35b74445a7b5dda12e38adc995dc1fae85b297da
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-01 00:00:42 +0100

    Get Local from Request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,6 +17,7 @@ class Config:
 
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale:
     """ Fuction to determine best match
@@ -27,7 +28,7 @@ def get_locale:
 @app.route('/')
 def index():
     """ Render the index.html page """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
