a08f674396f473bf9872749ca3bcef9e44a17230
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-04 00:53:19 +0100

    mock_logging_in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+""" A basic flask app """
+from flask import Flask, g, render_template, request
+from flask_babel import Babel
+from typing import Union
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config:
+    """ Configuration class for the application """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """ Fuction to determine best match
+    with supported languages """
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user() -> Union[Dict, None]:
+    """ A user module that returns a user when found"""
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """ Function that retrievess the login_as param from url"""
+
+    user = get_user()
+    g.user = user
+
+
+@app.route('/')
+def index():
+    """ Render the index.html page """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
